4f505420ece11a1164b78d759418da824a3876bd
CansuMammadova <cansummmdv@gmail.com>
2024-01-15 23:46:29 +0400

    updated

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -107,6 +107,225 @@ const Home = () => {
           </div>
         </div>
       </div>
+      <div className='popular-items section-padding30'>
+        <div className='container'>
+          <div className='row justify-content-center'>
+            <div className='col-xl-7 col-lg-8 col-md-10'>
+              <div className='section-tittle mb-70 text-center'>
+                <h2>Popular Items</h2>
+                <p>Consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Quis ipsum suspendisse ultrices gravida.</p>
+              </div>
+            </div>
+          </div>
+          <div className='row'>
+            <div className='col-xl-4 col-lg-4 col-md-6 col-sm-6'>
+              <div className='single-popular-items'>
+                <div className='popular-img'>
+                  <img src="/img/popular1.png.webp" alt="" />
+                  <div className='img-cap'>
+                    <span>Add to cart</span>
+                  </div>
+                  <div className='favorit-items'>
+                    <span className='flaticon-heart'></span>
+                  </div>
+                </div>
+                <div className='popular-caption'>
+                  <h3>
+                    <a href="">Thermo Ball Etip Gloves</a>
+                  </h3>
+                  <span>$ 45,743</span>
+                </div>
+              </div>
+            </div>
+            <div className='col-xl-4 col-lg-4 col-md-6 col-sm-6'>
+              <div className='single-popular-items'>
+                <div className='popular-img'>
+                  <img src="/img/popular2.png.webp" alt="" />
+                  <div className='img-cap'>
+                    <span>Add to cart</span>
+                  </div>
+                  <div className='favorit-items'>
+                    <span className='flaticon-heart'></span>
+                  </div>
+                </div>
+                <div className='popular-caption'>
+                  <h3>
+                    <a href="">Thermo Ball Etip Gloves</a>
+                  </h3>
+                  <span>$ 45,743</span>
+                </div>
+              </div>
+            </div>
+            <div className='col-xl-4 col-lg-4 col-md-6 col-sm-6'>
+              <div className='single-popular-items'>
+                <div className='popular-img'>
+                  <img src="/img/popular3.png.webp" alt="" />
+                  <div className='img-cap'>
+                    <span>Add to cart</span>
+                  </div>
+                  <div className='favorit-items'>
+                    <span className='flaticon-heart'></span>
+                  </div>
+                </div>
+                <div className='popular-caption'>
+                  <h3>
+                    <a href="">Thermo Ball Etip Gloves</a>
+                  </h3>
+                  <span>$ 45,743</span>
+                </div>
+              </div>
+            </div>
+            <div className='col-xl-4 col-lg-4 col-md-6 col-sm-6'>
+              <div className='single-popular-items'>
+                <div className='popular-img'>
+                  <img src="/img/popular4.png.webp" alt="" />
+                  <div className='img-cap'>
+                    <span>Add to cart</span>
+                  </div>
+                  <div className='favorit-items'>
+                    <span className='flaticon-heart'></span>
+                  </div>
+                </div>
+                <div className='popular-caption'>
+                  <h3>
+                    <a href="">Thermo Ball Etip Gloves</a>
+                  </h3>
+                  <span>$ 45,743</span>
+                </div>
+              </div>
+            </div>
+            <div className='col-xl-4 col-lg-4 col-md-6 col-sm-6'>
+              <div className='single-popular-items'>
+                <div className='popular-img'>
+                  <img src="/img/popular5.png.webp" alt="" />
+                  <div className='img-cap'>
+                    <span>Add to cart</span>
+                  </div>
+                  <div className='favorit-items'>
+                    <span className='flaticon-heart'></span>
+                  </div>
+                </div>
+                <div className='popular-caption'>
+                  <h3>
+                    <a href="">Thermo Ball Etip Gloves</a>
+                  </h3>
+                  <span>$ 45,743</span>
+                </div>
+              </div>
+            </div>
+            <div className='col-xl-4 col-lg-4 col-md-6 col-sm-6'>
+              <div className='single-popular-items'>
+                <div className='popular-img'>
+                  <img src="/img/popular6.png.webp" alt="" />
+                  <div className='img-cap'>
+                    <span>Add to cart</span>
+                  </div>
+                  <div className='favorit-items'>
+                    <span className='flaticon-heart'></span>
+                  </div>
+                </div>
+                <div className='popular-caption'>
+                  <h3>
+                    <a href="">Thermo Ball Etip Gloves</a>
+                  </h3>
+                  <span>$ 45,743</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className='row justify-content-center'>
+            <div className='room-btn pt-70'>
+              <a href="" className='btn view-btn1'>View More Products</a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='video-area mb-100'>
+        <div className='container-fluid'>
+          <div className='row align-items-center'>
+            <div className='col-lg-12'>
+              <div className='video-wrap'>
+                <div className='play-btn'>
+                  <a className='popup-video'
+                    href="https://www.youtube.com/watch?v=KMc6DyEJp04">
+                    <i className='fas fa-play'></i></a></div>
+              </div>
+            </div>
+          </div>
+          <div className='thumb-content-box'>
+            <div className='thumb-content'>
+              <h3>Next Video</h3>
+              <a href="#">
+                <i className='flaticon-arrow'></i>
+              </a>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='watch-area section-padding30'>
+        <div className='container'>
+          <div className='row align-items-center justify-content-between padding-130'>
+            <div className='col-lg-5 col-md-6'>
+              <div className='watch-details mb-40'>
+                <h2>Watch of Choice</h2>
+                <p>Enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                  Duis aute irure dolor in reprehenderit in voluptate velit esse.</p>
+                <a href="shop.html" className='btn'>Show Watches</a>
+              </div>
+            </div>
+            <div className='col-lg-5 col-md-6 col-sm-10'>
+              <div className='choice-watch-img mb-40'>
+                <img src="./img/popular6.png.webp" alt="" />
+              </div>
+            </div>
+          </div>
+          <div className='row align-items-center justify-content-between'>
+            <div className='col-lg-6 col-md-6 col-sm-10'>
+              <div className='choice-watch-img mb-40'>
+                <img src="./img/popular4.png.webp" alt="" />
+              </div>
+            </div>
+            <div className='col-lg-6 col-md-6'>
+              <div className='watch-details mb-40'>
+                <h2>Watch of Choice</h2>
+                <p>Enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+                  Duis aute irure dolor in reprehenderit in voluptate velit esse.</p>
+                  <a href="shop.html" className='btn'>Show Watches</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className='shop-method-area'>
+        <div className='container2'>
+          <div className='method-wrapper'>
+            <div className='row d-flex justify-content-between'>
+              <div className='col-xl-4 col-lg-4 col-md-6'>
+                <div className='single-method mb-40'>
+                  <i className='ti-package'></i>
+                  <h6>Free Shipping Method</h6>
+                  <p>aorem ixpsacdolor sit ameasecur adipisicing elitsf edasd.</p>
+                </div>
+              </div>
+              <div className='col-xl-4 col-lg-4 col-md-6'>
+                <div className='single-method mb-40'>
+                  <i className='ti-unlock'></i>
+                  <h6>Secure Payment System</h6>
+                  <p>aorem ixpsacdolor sit ameasecur adipisicing elitsf edasd.</p>
+                </div>
+              </div>
+              <div className='col-xl-4 col-lg-4 col-md-6'>
+                <div className='single-method mb-40'>
+                  <i className='ti-reload'></i>
+                  <h6>Secure Payment System</h6>
+                  <p>aorem ixpsacdolor sit ameasecur adipisicing elitsf edasd.</p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </Layout>
   )
 }
--- src/Pages/Shop/index.jsx
+++ src/Pages/Shop/index.jsx
@@ -4,195 +4,196 @@ import Layout from '../../Components/Layout';
 
 const Shop = () => {
   return (
-    <Layout><div className='shop'>
-      <div className='slider'>
-        <h2>Watch Shop</h2>
-      </div>
-      <div className='popular-items'>
-        <div className='container'>
-          <div className='row product-btn justify-content-between mb-40'>
-            <div className='properties-button'>
-              <nav>
-                <div className='nav-tabs'>
-                  <a href="#" className='nav-link'>NewestArrival</a>
-                  <a href="#" className='nav-link'>Price High To Low</a>
-                  <a href="#" className='nav-link'>Most Popular</a>
-                </div>
-              </nav>
+    <Layout>
+      <div className='shop'>
+        <div className='slider'>
+          <h2>Watch Shop</h2>
+        </div>
+        <div className='popular-items'>
+          <div className='container'>
+            <div className='row product-btn justify-content-between mb-40'>
+              <div className='properties-button'>
+                <nav>
+                  <div className='nav-tabs'>
+                    <a href="#" className='nav-link'>NewestArrival</a>
+                    <a href="#" className='nav-link'>Price High To Low</a>
+                    <a href="#" className='nav-link'>Most Popular</a>
+                  </div>
+                </nav>
+              </div>
+              <div className='select-this'>
+                <form action="">
+                  <div className='select-items'>
+                    <select className='select'>
+                      <option value="1">40 per page</option>
+                      <option value="2">50 per page</option>
+                      <option value="3">60 per page</option>
+                      <option value="4">70 per page</option>
+                    </select>
+                    <div className='select-open'>
+                      <span className='current'>
+                        <ul className='list'>
+                          <li className='option'>40 per page</li>
+                          <li className='option'>50 per page</li>
+                          <li className='option'>60 per page</li>
+                          <li className='option'>70 per page</li>
+                        </ul>
+                      </span>
+                    </div>
+                  </div>
+                </form>
+              </div>
             </div>
-            <div className='select-this'>
-              <form action="">
-                <div className='select-items'>
-                  <select className='select'>
-                    <option value="1">40 per page</option>
-                    <option value="2">50 per page</option>
-                    <option value="3">60 per page</option>
-                    <option value="4">70 per page</option>
-                  </select>
-                  <div className='select-open'>
-                    <span className='current'>
-                      <ul className='list'>
-                        <li className='option'>40 per page</li>
-                        <li className='option'>50 per page</li>
-                        <li className='option'>60 per page</li>
-                        <li className='option'>70 per page</li>
-                      </ul>
-                    </span>
+            <div className='tab-content'>
+              <div className='row'>
+                <div className='col-xl-4 col-lg-4 col-md-6 col-sm-6'>
+                  <div className='single-popular-items'>
+                    <div className='popular-img'>
+                      <img src="/img/popular1.png.webp" alt="" />
+                      <div className='img-cap'>
+                        <span>Add to cart</span>
+                      </div>
+                      <div className='favorit-items'>
+                        <span className='flaticon-heart'></span>
+                      </div>
+                    </div>
+                    <div className='popular-caption'>
+                      <h3>
+                        <a href="">Thermo Ball Etip Gloves</a>
+                      </h3>
+                      <span>$ 45,743</span>
+                    </div>
                   </div>
                 </div>
-              </form>
-            </div>
-          </div>
-          <div className='tab-content'>
-            <div className='row'>
-              <div className='col-xl-4 col-lg-4 col-md-6 col-sm-6'>
-                <div className='single-popular-items'>
-                  <div className='popular-img'>
-                    <img src="/img/popular1.png.webp" alt="" />
-                    <div className='img-cap'>
-                      <span>Add to cart</span>
+                <div className='col-xl-4 col-lg-4 col-md-6 col-sm-6'>
+                  <div className='single-popular-items'>
+                    <div className='popular-img'>
+                      <img src="/img/popular2.png.webp" alt="" />
+                      <div className='img-cap'>
+                        <span>Add to cart</span>
+                      </div>
+                      <div className='favorit-items'>
+                        <span className='flaticon-heart'></span>
+                      </div>
                     </div>
-                    <div className='favorit-items'>
-                      <span className='flaticon-heart'></span>
+                    <div className='popular-caption'>
+                      <h3>
+                        <a href="">Thermo Ball Etip Gloves</a>
+                      </h3>
+                      <span>$ 45,743</span>
                     </div>
-                  </div>
-                  <div className='popular-caption'>
-                    <h3>
-                      <a href="">Thermo Ball Etip Gloves</a>
-                    </h3>
-                    <span>$ 45,743</span>
                   </div>
                 </div>
-              </div>
-              <div className='col-xl-4 col-lg-4 col-md-6 col-sm-6'>
-                <div className='single-popular-items'>
-                  <div className='popular-img'>
-                    <img src="/img/popular2.png.webp" alt="" />
-                    <div className='img-cap'>
-                      <span>Add to cart</span>
+                <div className='col-xl-4 col-lg-4 col-md-6 col-sm-6'>
+                  <div className='single-popular-items'>
+                    <div className='popular-img'>
+                      <img src="/img/popular3.png.webp" alt="" />
+                      <div className='img-cap'>
+                        <span>Add to cart</span>
+                      </div>
+                      <div className='favorit-items'>
+                        <span className='flaticon-heart'></span>
+                      </div>
                     </div>
-                    <div className='favorit-items'>
-                      <span className='flaticon-heart'></span>
+                    <div className='popular-caption'>
+                      <h3>
+                        <a href="">Thermo Ball Etip Gloves</a>
+                      </h3>
+                      <span>$ 45,743</span>
                     </div>
-                  </div>
-                  <div className='popular-caption'>
-                    <h3>
-                      <a href="">Thermo Ball Etip Gloves</a>
-                    </h3>
-                    <span>$ 45,743</span>
                   </div>
                 </div>
-              </div>
-              <div className='col-xl-4 col-lg-4 col-md-6 col-sm-6'>
-                <div className='single-popular-items'>
-                  <div className='popular-img'>
-                    <img src="/img/popular3.png.webp" alt="" />
-                    <div className='img-cap'>
-                      <span>Add to cart</span>
+                <div className='col-xl-4 col-lg-4 col-md-6 col-sm-6'>
+                  <div className='single-popular-items'>
+                    <div className='popular-img'>
+                      <img src="/img/popular4.png.webp" alt="" />
+                      <div className='img-cap'>
+                        <span>Add to cart</span>
+                      </div>
+                      <div className='favorit-items'>
+                        <span className='flaticon-heart'></span>
+                      </div>
                     </div>
-                    <div className='favorit-items'>
-                      <span className='flaticon-heart'></span>
+                    <div className='popular-caption'>
+                      <h3>
+                        <a href="">Thermo Ball Etip Gloves</a>
+                      </h3>
+                      <span>$ 45,743</span>
                     </div>
-                  </div>
-                  <div className='popular-caption'>
-                    <h3>
-                      <a href="">Thermo Ball Etip Gloves</a>
-                    </h3>
-                    <span>$ 45,743</span>
                   </div>
                 </div>
-              </div>
-              <div className='col-xl-4 col-lg-4 col-md-6 col-sm-6'>
-                <div className='single-popular-items'>
-                  <div className='popular-img'>
-                    <img src="/img/popular4.png.webp" alt="" />
-                    <div className='img-cap'>
-                      <span>Add to cart</span>
+                <div className='col-xl-4 col-lg-4 col-md-6 col-sm-6'>
+                  <div className='single-popular-items'>
+                    <div className='popular-img'>
+                      <img src="/img/popular5.png.webp" alt="" />
+                      <div className='img-cap'>
+                        <span>Add to cart</span>
+                      </div>
+                      <div className='favorit-items'>
+                        <span className='flaticon-heart'></span>
+                      </div>
                     </div>
-                    <div className='favorit-items'>
-                      <span className='flaticon-heart'></span>
+                    <div className='popular-caption'>
+                      <h3>
+                        <a href="">Thermo Ball Etip Gloves</a>
+                      </h3>
+                      <span>$ 45,743</span>
                     </div>
-                  </div>
-                  <div className='popular-caption'>
-                    <h3>
-                      <a href="">Thermo Ball Etip Gloves</a>
-                    </h3>
-                    <span>$ 45,743</span>
                   </div>
                 </div>
-              </div>
-              <div className='col-xl-4 col-lg-4 col-md-6 col-sm-6'>
-                <div className='single-popular-items'>
-                  <div className='popular-img'>
-                    <img src="/img/popular5.png.webp" alt="" />
-                    <div className='img-cap'>
-                      <span>Add to cart</span>
+                <div className='col-xl-4 col-lg-4 col-md-6 col-sm-6'>
+                  <div className='single-popular-items'>
+                    <div className='popular-img'>
+                      <img src="/img/popular6.png.webp" alt="" />
+                      <div className='img-cap'>
+                        <span>Add to cart</span>
+                      </div>
+                      <div className='favorit-items'>
+                        <span className='flaticon-heart'></span>
+                      </div>
                     </div>
-                    <div className='favorit-items'>
-                      <span className='flaticon-heart'></span>
+                    <div className='popular-caption'>
+                      <h3>
+                        <a href="">Thermo Ball Etip Gloves</a>
+                      </h3>
+                      <span>$ 45,743</span>
                     </div>
-                  </div>
-                  <div className='popular-caption'>
-                    <h3>
-                      <a href="">Thermo Ball Etip Gloves</a>
-                    </h3>
-                    <span>$ 45,743</span>
-                  </div>
-                </div>
-              </div>
-              <div className='col-xl-4 col-lg-4 col-md-6 col-sm-6'>
-                <div className='single-popular-items'>
-                  <div className='popular-img'>
-                    <img src="/img/popular6.png.webp" alt="" />
-                    <div className='img-cap'>
-                      <span>Add to cart</span>
-                    </div>
-                    <div className='favorit-items'>
-                      <span className='flaticon-heart'></span>
-                    </div>
-                  </div>
-                  <div className='popular-caption'>
-                    <h3>
-                      <a href="">Thermo Ball Etip Gloves</a>
-                    </h3>
-                    <span>$ 45,743</span>
                   </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-      <div className='shop-method-area'>
-        <div className='container'>
-          <div className='method-wrapper'>
-            <div className='row d-flex justify-content-between'>
-              <div className='col-xl-4 col-lg-4 col-md-6'>
-                <div className='single-method mb-40'>
-                  <i className='ti-package'></i>
-                  <h6>Free Shipping Method</h6>
-                  <p>aorem ixpsacdolor sit ameasecur adipisicing elitsf edasd.</p>
+        <div className='shop-method-area'>
+          <div className='container'>
+            <div className='method-wrapper'>
+              <div className='row d-flex justify-content-between'>
+                <div className='col-xl-4 col-lg-4 col-md-6'>
+                  <div className='single-method mb-40'>
+                    <i className='ti-package'></i>
+                    <h6>Free Shipping Method</h6>
+                    <p>aorem ixpsacdolor sit ameasecur adipisicing elitsf edasd.</p>
+                  </div>
                 </div>
-              </div>
-              <div className='col-xl-4 col-lg-4 col-md-6'>
-                <div className='single-method mb-40'>
-                  <i className='ti-unlock'></i>
-                  <h6>Secure Payment System</h6>
-                  <p>aorem ixpsacdolor sit ameasecur adipisicing elitsf edasd.</p>
+                <div className='col-xl-4 col-lg-4 col-md-6'>
+                  <div className='single-method mb-40'>
+                    <i className='ti-unlock'></i>
+                    <h6>Secure Payment System</h6>
+                    <p>aorem ixpsacdolor sit ameasecur adipisicing elitsf edasd.</p>
+                  </div>
                 </div>
-              </div>
-              <div className='col-xl-4 col-lg-4 col-md-6'>
-                <div className='single-method mb-40'>
-                  <i className='ti-reload'></i>
-                  <h6>Secure Payment System</h6>
-                  <p>aorem ixpsacdolor sit ameasecur adipisicing elitsf edasd.</p>
+                <div className='col-xl-4 col-lg-4 col-md-6'>
+                  <div className='single-method mb-40'>
+                    <i className='ti-reload'></i>
+                    <h6>Secure Payment System</h6>
+                    <p>aorem ixpsacdolor sit ameasecur adipisicing elitsf edasd.</p>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
-    </div></Layout>
+      </div></Layout>
   )
 }
 
